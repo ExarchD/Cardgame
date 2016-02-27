1666f6fd9d113d509b33b6ba551bcded7474d702
Dan Pluth <pluthd@gmail.com>
2016-02-27 01:33:22 +0100

    uses password hashing now

--- home.php
+++ home.php
@@ -1,31 +1,35 @@
 <?php
-session_start();
-include_once 'dbconnect.php';
-
-if(!isset($_SESSION['user']))
+include_once 'dbconfig.php';
+if(!$user->is_loggedin())
 {
- header("Location: index.php");
+ $user->redirect('index.php');
 }
-$res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
-$userRow=mysql_fetch_array($res);
+$user_id = $_SESSION['user_session'];
+$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
+$stmt->execute(array(":user_id"=>$user_id));
+$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Welcome - <?php echo $userRow['email']; ?></title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css"  />
+<title>welcome - <?php print($userRow['user_email']); ?></title>
 </head>
+
 <body>
-<div id="header">
- <div id="left">
-    <label>cleartuts</label>
+
+<div class="header">
+ <div class="left">
+     <label><a href="http://www.codingcage.com/">Coding Cage - Programming Blog</a></label>
     </div>
-    <div id="right">
-     <div id="content">
-         hi' <?php echo $userRow['username']; ?>&nbsp;<a href="logout.php?logout">Sign Out</a>
-        </div>
+    <div class="right">
+     <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
     </div>
+</div>
+<div class="content">
+welcome : <?php print($userRow['user_name']); ?>
 </div>
 </body>
 </html>
