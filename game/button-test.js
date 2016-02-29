a070acbaf1084ea59835160567f0567bb15a810f
Dan McDougall <dmcdouga@iastate.edu>
2016-02-28 22:25:16 -0600

    put classes at the top, with Class container formatting

--- game/button-test.js
+++ game/button-test.js
@@ -1,11 +1,65 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'button-test', { preload: preload, create: create });
 
+////// Start Card class //////
+
+	// Has variables suit and rank
+	// possible suits are c=clubs, s=spades, d=diamonds, h=hearts
+	// possible ranks are 2 through 14, where 11 = Jack, 12=Queen, 13=King, 14=Ace
+
+var Card = function Card(suit, rank)
+{
+	this.suit = suit;
+	this.rank = rank;
+};
+
+////// End Card class //////
+
+////// Start Deck class //////
+	// A deck is an array of cards.
+	// The default constructor makes a deck with 52 cards
+	// There is also a constructor to make an empty deck
+	// Member functions for deck are:
+		// fillDeck()--Erases previous deck state and fills with 52 cards in order
+		// shuffleDeck()--Shuffles whatever cards are currently in the deck
+		// takeTopCard()--Returns the top card in the deck and removes it from the deck
+		// addOneCard()--Adds one card top of deck
+
+var Deck = function Deck()
+{
+	//default constructor, calls fillDeck.
+};
+
+Deck.prototype.fillDeck() = function()
+{
+	// Will fill the deck with 52 cards
+};
+
+Deck.prototype.shuffleDeck() = function()
+{
+	//Shuffles cards currently in deck
+
+};
+
+Deck.prototype.takeTopCard() = function()
+{
+	//Take top card from deck, pop function in array
+};
+
+Deck.prototype.addOneCard() = function ()
+{
+	//Add one card
+};
+
+////// End Deck class //////
+
+
 function preload() {
 
 	game.load.spritesheet('button', 'assets/button_sprite_sheet.png', 193, 71);
 	game.load.image('background','assets/starfield.jpg');
 
 }
+
 var card_number = 3; // Number of cards on screen, will be buttons
 var button = new Array(); //Array of butons for screen
 var background; // back ground slide
@@ -34,59 +88,7 @@ function create() {
 		button[i].onInputOut.add(out, this);
 		button[i].onInputUp.add(up, this);
     }
-
-
-
 }
-
-
-// card class
-// Has variables suit and rank
-// possible suits are c=clubs, s=spades, d=diamonds, h=hearts
-// possible ranks are 2 through 14, where 11 = Jack, 12=Queen, 13=King, 14=Ace
-
-var Card = function Card(suit, rank)
-{
-	this.suit = suit;
-	this.rank = rank;
-};
-
-
-// A deck is a group of cards.
-// The default constructor makes a deck with 52 cards
-// There is also a constructor to make an empty deck
-// Member functions for deck are:
-// fillDeck()--Erases previous deck state and fills with 52 cards in order
-// shuffleDeck()--Shuffles whatever cards are currently in the deck
-// takeTopCard()--Returns the top card in the deck and removes it from the deck
-// addOneCard()--Adds one card top of deck
-
-var Deck = function Deck()
-{
-	//default constructor, calls fillDeck.
-};
-
-Deck.prototype.fillDeck() = function()
-{
-	// Will fill the deck with 52 cards
-};
-
-Deck.prototype.shuffleDeck() = function()
-{
-	//Shuffles cards currently in deck
-
-};
-
-Deck.prototype.takeTopCard() = function()
-{
-	//Take top card from deck, pop function in array
-};
-
-Deck.prototype.addOneCard() = function ()
-{
-	//Add one card
-};
-
 
 
 function up() {
