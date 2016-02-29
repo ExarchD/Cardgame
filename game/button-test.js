aa2023a5c9c56d34cbb4a38f5dca534b9e0fcdee
M T Hersh <mthersh@iastate.edu>
2016-02-28 19:08:25 -0600

    Testing Comments

--- game/button-test.js
+++ game/button-test.js
@@ -6,19 +6,20 @@ function preload() {
     game.load.image('background','assets/starfield.jpg');
 
 }
-var card_number = 5;
-var button = new Array();
-var background;
+var card_number = 5; // Number of cards on screen
+var button = new Array(); //Array of butons for screen
+var background; // back ground slide
 
 function create() {
 
-    game.stage.backgroundColor = '#182d3b';
+    game.stage.backgroundColor = '#182d3b'; // Hex code color 
 
+	
     background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
     for (i=0; i < card_number; i++)
     {
-	    var number = 45 + 40*i;
+	    var number = 45 + 40*i; //spacing is 40 pixels
     button[i] = game.add.button(game.world.centerX - number, 400, 'button', actionOnClick, this, 2, 1, 0);
     button[i].onInputOver.add(over, this);
     button[i].onInputOut.add(out, this);
@@ -27,6 +28,9 @@ function create() {
 
 
 }
+
+
+
 
 function up() {
     console.log('button up', arguments);
