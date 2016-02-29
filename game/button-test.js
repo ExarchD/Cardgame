24fec2fc8afd64c5e8c5a50299e72e4059449116
M T Hersh <mthersh@iastate.edu>
2016-02-28 21:34:10 -0600

    no message

--- game/button-test.js
+++ game/button-test.js
@@ -6,7 +6,7 @@ function preload() {
 	game.load.image('background','assets/starfield.jpg');
 
 }
-var card_number = 5; // Number of cards on screen
+var card_number = 3; // Number of cards on screen, will be buttons
 var button = new Array(); //Array of butons for screen
 var background; // back ground slide
 
@@ -17,13 +17,21 @@ function create() {
 	
     background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
+	// Variables X and Y locations for Your Cards
+	
+	var xPixFromCenter;
+	var yPix = 400;
+	
+	
     for (i=0; i < card_number; i++)
     {
-	    var number = 45 + 40*i; //spacing is 40 pixels
-    button[i] = game.add.button(game.world.centerX - number, 400, 'button', actionOnClick, this, 2, 1, 0);
-    button[i].onInputOver.add(over, this);
-    button[i].onInputOut.add(out, this);
-    button[i].onInputUp.add(up, this);
+	    var xPixFromCenter = 45 + 40*i; //spacing is 40 pixels
+		
+		//Button constructor, upFrame is left blank, not used
+		button[i] = game.add.button(game.world.centerX - xPixFromCenter, yPix, 'button', actionOnClick, this, 2, 1, 0);
+		button[i].onInputOver.add(over, this);
+		button[i].onInputOut.add(out, this);
+		button[i].onInputUp.add(up, this);
     }
 
 
