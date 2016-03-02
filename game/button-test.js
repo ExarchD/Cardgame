01ff3171285f4e3175f1894ae905fe287bf2024c
M T Hersh <mthersh@iastate.edu>
2016-03-01 22:50:39 -0600

    Fixed issues with "var" vs. "this" scope, added player variables and functions

--- game/button-test.js
+++ game/button-test.js
@@ -3,15 +3,15 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'button-test', { preload: prel
 ////// Start Card class //////
 
 // Member variables for Card class:
-// suit 
-// rank
+//      suit 
+//      rank
 // possible suits are c=clubs, s=spades, d=diamonds, h=hearts
 // possible ranks are 2 through 14, where 11 = Jack, 12=Queen, 13=King, 14=Ace
 
-var Card = function Card(suit, rank)
+var Card = function Card(s, r)
 {
-	this.suit = suit;
-	this.rank = rank;
+	this.suit = s;
+	this.rank = r;
 };
 
 ////// End Card class //////
@@ -21,19 +21,19 @@ var Card = function Card(suit, rank)
 // The default constructor makes a deck with 52 cards
 // There is also a constructor to make an empty deck
 // Member variables for deck are:
-// cardArray[] (array of Card)
+//      cardArray[] (array of Card)
 // Member functions for deck are:
-// fillDeck()--Erases previous deck state and fills with 52 cards in order
-// fillDeck( totalSuit, totalRank)--Allows you to fill deck with custom numbers of suit and rank
-// shuffleDeck()--Shuffles whatever cards are currently in the deck
-// takeTopCard()--Returns the top card in the deck and removes it from the deck
-// addOneCard()--Adds one card top of deck
-// takeIndexCard( cardIndex )--Removes a card at the index given
+//      fillDeck()--Erases previous deck state and fills with 52 cards in order
+//      fillDeck( totalSuit, totalRank)--Allows you to fill deck with custom numbers of suit and rank
+//      shuffleDeck()--Shuffles whatever cards are currently in the deck
+//      takeTopCard()--Returns the top card in the deck and removes it from the deck
+//      addCards()--Adds card top of deck, if array of cards will add all cards
+//      takeIndexCard( cardIndex )--Removes a card at the index given
 
 //Default constuctor: Makes an empty deck of cards
 var Deck = function Deck()
 {
-	var cardArray = new Array();
+	this.cardArray = [];
 };
 
 // fillDeck()
@@ -41,13 +41,16 @@ var Deck = function Deck()
 Deck.prototype.fillDeck() = function()
 {
 	// Array for suit characters
+	// Default 0 = c     1 = s    2 = d    3 = h
 	var suitArray = [c, s, d, h];
+	var minRank = 2; //Minimum card is a 2
+	var maxRank = 14; //Max rank is Ace = 14
 
 	//First For loop: cycles through the suits	
 	//Second For loop: cycles through the ranks
 	for (fillSuits=0; fillSuits < 3; fillSuits++)
 	{
-		for (fillRanks=2; fillRanks < 15; fillRanks++)
+		for (fillRanks=minRank; fillRanks < maxRank + 1; fillRanks++)
 		{
 			this.cardArray.add(new Card(suitArray[fillSuits], fillRanks));
 		}
@@ -92,18 +95,18 @@ Deck.prototype.fillDeck(totalSuit, totalRank) = function()
 Deck.prototype.shuffleDeck() = function()
 {
 	// Makes a temp card so you can swap
-	tempCard = new Card (0,0);
+	var tempCard = new Card (0,0);
 	// The randomly selected card in the deck that the current card will switch with
 	var cardToSwitchWith;
 
-	for (shuffleThisCard = 0; shuffleThisCard < cardArray.length; shuffleThisCard++)
+	for (shuffleThisCard = 0; shuffleThisCard < this.cardArray.length; shuffleThisCard++)
 	{
 		//Giving random number between 1 and number of cards in deck
-		cardToSwitchWith = Math.floor((Math.random() * cardArray.length) + 1);
+		cardToSwitchWith = Math.floor((Math.random() * this.cardArray.length) + 1);
 		// Next 3 lines are swap
-		tempCard = cardArray[shuffleThisCard];
-		cardArray[shuffleThisCard] = cardArray[cardToSwitchWith - 1]; // The -1 switches from # to index
-		cardArray[cardToSwitchWith - 1] = cardArray[shuffleThisCard];		
+		tempCard = this.cardArray[shuffleThisCard];
+		this.cardArray[shuffleThisCard] = this.cardArray[cardToSwitchWith - 1]; // The -1 switches from # to index
+		this.cardArray[cardToSwitchWith - 1] = this.cardArray[shuffleThisCard];		
 	}
 };
 
@@ -111,21 +114,22 @@ Deck.prototype.shuffleDeck() = function()
 // This removes the top card (last in array) from the deck
 Deck.prototype.takeTopCard() = function()
 {
-	return cardArray.pop();
+	return this.cardArray.pop();
 };
 
-// addCard( cardToAdd) 
-// This adds a card to the top of the deck (adds to last in array)
-Deck.prototype.addCard( cardToAdd ) = function ()
+// addCards( cardToAdd) 
+// This adds a card(s) to the top of the deck (adds to last in array)
+// NOTE: Will work if either single card or array of cards is added
+Deck.prototype.addCards( cardsToAdd ) = function ()
 {
-	cardArray.push( cardToAdd );
+	this.cardArray.concat( cardsToAdd );
 };
 
 // takeIndexCard( cardIndex ) 
 // This removes a card at a particular index and returns it
 Deck.prototype.takeIndexCard( deckCardIndex ) = function ()
 {
-	return cardArray.splice(cardIndex, 1);
+	return this.cardArray.splice(cardIndex, 1);
 };
 
 ////// End Deck class //////
@@ -137,34 +141,55 @@ Deck.prototype.takeIndexCard( deckCardIndex ) = function ()
 // A player is an object that can receive cards and play cards from their hand (Deck). The board is 
 // printed based on what the player can see
 // Member variables for Player Class
-// myHand = new Deck()--The cards in the player's hand
-// myCardsWon = new Deck()--The cards from the tricks the player has won
+//      myHand = new Deck()--The cards in the player's hand
+//      myCardsWon = new Deck()--The cards from the tricks the player has won
+//      myPiles = new Deck()-- In particular games a player might need more piles of cards, this is an array of decks
 // Member functions for Player Class
-// receiveCard( cardToAdd ) 
-// playCard()
-// sortHand()
-// printScreen()
+//      receiveCardHand( cardToAdd ) 
+//      receiveCardWin( cardToAdd )
+//      playCardHand( handCardIndex)
+//      emptyAllDecks()
+//      sortHand()
 
 //Default constuctor: Makes a player with an empty deck of cards
 var Player = function Player()
 {
-	var myHand = new Deck();
+	this.myHand = new Deck();
+	this.myCardsWon = new Deck();
+	this.myPiles = [];
 };
 
-// receiveCard( cardToAdd )
-// Function used to give a player a card
-Player.prototype.receiveCard( cardToAdd ) = function ()
+// receiveCardHand( cardToAdd )
+// Function used to give a player a card or set of cards (this function will accept arrays)
+Player.prototype.receiveCardHand( cardToAdd ) = function ()
 {
-	this.myHand.addCard( cardToAdd );
+	this.myHand.addCards( cardToAdd );
+};
+
+// receiveCardWin( cardToAdd )
+// Function used to give a player a card or set of cards (this function will accept arrays)
+Player.prototype.receiveCardWin( cardToAdd ) = function ()
+{
+	this.myCardsWon.addCards( cardToAdd );
 };
 
 // playCard( handCardIndex )
 // Function used to play a card, will be called by clicking on card button
 // By clicking on the card button the index of the card clicked will be passed to this function
-Player.prototype.playCard( handCardIndex ) = function ()
+Player.prototype.playCardHand( handCardIndex ) = function ()
 {
 	return this.myHand.takeIndexCard( handCardIndex );
 };
+
+// emptyAllDecks()
+// Function used to get rid of all cards a player has
+Player.prototype.emptyAllDecks() = function ()
+{
+	this.myHand.splice(0,myHand.length);
+	this.myCardsWon.splice(0,myCardsWon.length);
+	this.myPiles.splice(0,myPiles.length);
+};
+
 
 // sortHand()
 // Function used to sort a hand, can be called by button on player screen
@@ -173,12 +198,7 @@ Player.prototype.sortHand() = function ()
 	// Code that sorts hand, give deck a function that sorts???
 };
 
-Player.prototype.printScreen() = function ()
-{
-	// Code that prints screen, will likely need access to some data (score, round, trump) from game
-	// SHOULD THIS BE IN PLAYER OR IN GAME?
 
-};
 
 ////// End Player class //////
 
@@ -191,6 +211,7 @@ Player.prototype.printScreen() = function ()
 // 2) See if a new round should start based on win conditions and set up that round 
 //     (shuffle, deal, bidding, cardplay)
 // 3) Check that all moves are legal during the round (trump, following suit)
+//      3a) Check that the game isn't instantly won after a trick, ie team with 10 gets a 2 of trump played
 // 4) Tablulate and update scores at the end of the round
 
 
